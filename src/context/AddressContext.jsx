c08035323f0565bfb98041c56ac161f0b00385d2
vishal <vishalray7891@gmail.com>
2025-12-12 15:34:36 +0530

    Update AddressContext.jsx

--- src/context/AddressContext.jsx
+++ src/context/AddressContext.jsx
@@ -1,5 +1,48 @@
+// import { createContext, useContext, useEffect, useState } from "react";
+// import axios from "axios";
+
+// const AddressContext = createContext();
+
+// export const useAddress = () => useContext(AddressContext);
+
+// export const AddressProvider = ({ children }) => {
+//   const [addresses, setAddresses] = useState([]);
+//   const token = localStorage.getItem("token");
+
+//   const fetchAddresses = async () => {
+//     try {
+//       const res = await axios.get("http://localhost:5000/api/addresses", {
+//         headers: { Authorization: `Bearer ${token}` },
+//       });
+//       setAddresses(res.data.addresses);
+//     } catch (err) {
+//       console.error("Fetch address error", err);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (token) fetchAddresses();
+//   }, [token]);
+
+//   return (
+//     <AddressContext.Provider value={{ addresses, setAddresses, fetchAddresses }}>
+//       {children}
+//     </AddressContext.Provider>
+//   );
+// };
+
+
+
+
+
+
+// with backend url     
+
+
+
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
+import { API_URL } from "../config";
 
 const AddressContext = createContext();
 
@@ -11,7 +54,7 @@ export const AddressProvider = ({ children }) => {
 
   const fetchAddresses = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/addresses", {
+      const res = await axios.get(`${API_URL}/api/addresses`, {
         headers: { Authorization: `Bearer ${token}` },
       });
       setAddresses(res.data.addresses);
