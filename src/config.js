bace552db6c63b486bad70532174424b220a9103
vishal <vishalray7891@gmail.com>
2025-12-18 16:58:51 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,6 @@
-export const API_URL = "https://e-commerce-backend-4-vplk.onrender.com";
+// export const API_URL = "https://e-commerce-backend-4-vplk.onrender.com";
+
+
+
+// src/config.js
+export const API_URL = "https://e-commerce-backend-4-vplk.onrender.com/api";
